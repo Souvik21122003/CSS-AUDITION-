521b58d9974ba0020b5c92ed3791c0230057a789
Souvik Dey <shoredasherdey680@gmail.com>
2024-10-20 19:40:04 +0530

    msg2

--- Frontend/audition/src/Info.jsx
+++ Frontend/audition/src/Info.jsx
@@ -115,11 +115,11 @@ function Info() {
   fullWidth
   error={
     // Updated regex to match both formats like 'msr.22cs8043@nitdgp.ac.in' and 'sd.23u10156@nitdgp.ac.in'
-    !/^[a-z]+(\.[a-z]+)*\.\d{2}[a-z]{1,2}\d{5}@nitdgp\.ac\.in$/.test(formData.email) && formData.email !== ""
+    !/^[a-z]+(\.[a-z]+)*\.\d{2}[a-z]{1,2}\d{4}@nitdgp\.ac\.in$/.test(formData.email) && formData.email !== ""
   }
   helperText={
     // Provide error message if email format is incorrect
-    !/^[a-z]+(\.[a-z]+)*\.\d{2}[a-z]{1,2}\d{5}@nitdgp\.ac\.in$/.test(formData.email) && formData.email !== ""
+    !/^[a-z]+(\.[a-z]+)*\.\d{2}[a-z]{1,2}\d{4}@nitdgp\.ac\.in$/.test(formData.email) && formData.email !== ""
     ? "Email format is incorrect, must provide college email address"
     : ""
   }
